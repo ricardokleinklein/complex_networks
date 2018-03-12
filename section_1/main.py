6d7a1e8836f3cf1ee408cd342155f9fc8606c121
Ricardo Kleinlein <ricardokleinlein@Ricardos-MacBook-Pro.local>
2018-03-12 23:20:02 +0900

    include main file

---
+++ section_1/main.py
@@ -0,0 +1,15 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import networkx as nx
+import docopt as docopt
+from tqdm import tqdm
+
+from utils import *
+
+if __name__ == '__main__':
+	
