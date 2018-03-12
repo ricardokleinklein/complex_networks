a49db96a867edd1761712b420ab5239f09656417
Ricardo Kleinlein <ricardokleinlein@Ricardos-MacBook-Pro.local>
2018-03-13 00:38:45 +0900

    get graph files, get nodes and edges

--- section_1/utils.py
+++ section_1/utils.py
@@ -6,8 +6,27 @@ import os
 import numpy as np
 import networkx as nx
 
-def _load_graph(path):
+def _rm_hidden(files):
+	return [file for file in files if not file.startswith(".")]
+
+def load_graph(path):
 	return nx.read_pajek(path)
+
+def compile_graph_libs(in_dir):
+	"""Return a dictionary with all the graphs available."""
+	dirs = _rm_hidden(os.listdir(in_dir))
+	graphs = dict()
+	for d in dirs:
+		g = _rm_hidden(os.listdir(
+			os.path.join(in_dir, d)))
+		graphs[d] = g
+	return graphs
+
+def get_num_vertices(G):
+	return nx.number_of_nodes(G)
+
+def get_num_edges(G):
+	return nx.number_of_edges(G)
 
 def draw_graph(G, name):
 	nx.draw_networkx(G, with_labels=True, font_weight='bold',
