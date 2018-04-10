b0cb6662be8b7bdd20a41fbcb6148f4f0b3ea5c1
Ricardo Kleinlein <ricardokleinklein@gmail.com>
2018-04-10 19:44:41 +0900

    configuration model in most naive way

--- section_2/utils.py
+++ section_2/utils.py
@@ -28,8 +28,7 @@ def _new_ER(G, N, p):
 	for idx, e in enumerate(non_edges):
 		if idx in edges_idx:
 			G.add_edge(e[0], e[1])
-	return G
-
+	
 
 def new_ER(N, K=None, p=None):
 	"""Erdos-Renyi network."""
@@ -56,20 +55,18 @@ def _make_ring_lattice(G, K):
 			op = abs(i - j) % d
 			if op > 0 and op <= K/2:
 				G.add_edge(i,j)
-	return G
 
 
 def _update_G(G, change):
 	G.remove_edge(change[0], change[1])
 	G.add_edge(change[0], change[2])
-	return G
 
 
 def new_WS(N, K, p):
 	"""Watts-Strogatz network."""
 	# It must satisfy that N >> K >> log(N) >> 1
 	G = _new_empty_G(N)
-	G = _make_ring_lattice(G, K)
+	_make_ring_lattice(G, K)
 	nodes = nx.nodes(G)
 	for i in nodes:
 		neighbors = nx.all_neighbors(G, i)
@@ -83,18 +80,17 @@ def new_WS(N, K, p):
 					new_j = np.random.randint(0, N)
 					while new_j in forbidden:
 						new_j = np.random.randint(0, N)
-					G = _update_G(G, (i, j, new_j))
+					_update_G(G, (i, j, new_j))
 	return G
 
 
-def _make_clique_G(G):
+def _make_clique(G):
 	non_edges = nx.non_edges(G)
 	for e in non_edges:
 		G.add_edge(e[0], e[1])
-	return G
 
 
-def _add_BA_node(G, m):
+def _add_node(G, m):
 	pdf = get_pdf(G)
 	new_node = len(pdf)
 	G.add_node(new_node)
@@ -113,9 +109,42 @@ def new_BA(N, m):
 	"""Barabasi and Albert network."""
 	N_init = 15
 	G = _new_empty_G(N_init)
-	G = _make_clique_G(G)
+	_make_clique(G)
 	while nx.number_of_nodes(G) < N:
-		_add_BA_node(G, m)
+		_add_node(G, m)
+	return G
+
+
+def _generate_random_pdf(N):
+	pdf = np.random.randint(0, N - 1, size= N)
+	while np.sum(pdf) % 2 != 0:
+		pdf = np.random.randint(0, N - 1, size=N)
+	return pdf
+
+
+def new_CM(N, pdf=None):
+	G = _new_empty_G(N)
+	max_n_edges = N - 1
+	if pdf is None:
+		pdf = _generate_random_pdf(N)
+	K = np.sum(pdf)
+	stubs_node = [np.sum(pdf[:i+1]) for i in range(len(pdf))]
+	print(K, pdf, stubs_node)
+	forbidden = list()
+	while len(forbidden) != K:
+		stub_i = np.random.randint(0, K)
+		while stub_i in forbidden:
+			stub_i = np.random.randint(0, K)
+		i = min([stubs_node.index(s) for s in stubs_node if s > stub_i])
+		forbidden.append(stub_i)
+
+		stub_j = np.random.randint(0, K)
+		while stub_j in forbidden:
+			stub_j = np.random.randint(0, K)
+		j = min([stubs_node.index(s) for s in stubs_node if s > stub_j])
+		forbidden.append(stub_j)
+		G.add_edge(i, j)
+
 	return G
 
 
